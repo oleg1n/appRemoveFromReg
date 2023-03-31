b3da4b0c0fa7c342acf82cdb8c7d502ea9440329
Oleg Kupriyanov <oleg1n@mail.ru>
2023-03-31 08:33:46 +0300

    commit#1 appRemoveFromReg

---
+++ src/main/java/org/kuprijanov/urr16/appRemoveFromReg/AppRemoveFromRegApplication.java
@@ -0,0 +1,13 @@
+package org.kuprijanov.urr16.appRemoveFromReg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRemoveFromRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRemoveFromRegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kuprijanov/urr16/appRemoveFromReg/AppRemoveFromRegApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kuprijanov.urr16.appRemoveFromReg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRemoveFromRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
